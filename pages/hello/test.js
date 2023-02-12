34c3e5df2f3ece28a09db03fd2fb8087b0957e32
Parth Shah <helloparthshah@gmail.com>
2023-02-11 23:31:10 -0800

    Updated index page for the example

--- pages/hello/test.js
+++ pages/hello/test.js
@@ -1,7 +1,16 @@
+import { useEffect, useState } from 'react'
+import Link from 'next/link'
 export default function Test() {
+    const [name, setName] = useState("Loading...")
+    useEffect(() => {
+        fetch('/api/hello').then(res => res.json()).then(data => {
+            setName(data.name)
+        })
+    }, [])
     return (
         <div>
-            <h1>Test</h1>
+            <h1>{name}</h1>
+            <h1><Link href="/">Home</Link></h1>
         </div>
     )
 }
