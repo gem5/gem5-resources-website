196e2591415b41e2f418444164297c0f22c81979
Parth Shah <helloparthshah@gmail.com>
2023-02-13 10:24:08 -0800

    fixed a bug with safari

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
           integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
         <link rel="shortcut icon" href="/favicon.png" />
       </Head>
