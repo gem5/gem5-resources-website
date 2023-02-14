6c97d385661392250e17dbc7562f8daef5de177a
Parth Shah <helloparthshah@gmail.com>
2023-02-13 19:26:56 -0800

    finished with meta data

--- components/resource-metadata.js
+++ components/resource-metadata.js
@@ -36,13 +36,18 @@ export default function MetaData({ resource }) {
                     <p className="primary">
                         {resource.description ?? 'This is a description of the resource.'}
                     </p>
+                    <Link
+                        href={resource.github_url ?? 'https://www.gem5.org/documentation/general_docs/resources'}
+                    >
+                        Repository (GitHub)
+                    </Link>
                 </Row>
                 <Row className="border-bottom">
                     <p className="text-muted">License</p>
                     <p className="primary">
                         {resource.license ?? 'BSD-3-Clause'}
                         {' ('}
-                        <Link className="primary"
+                        <Link
                             href={resource.license_url ?? 'https://opensource.org/licenses/BSD-3-Clause'}
                         >
                             LICENCE
