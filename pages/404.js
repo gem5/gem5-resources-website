5e9c712e24253744a5694066528265730a6b219a
Parth Shah <helloparthshah@gmail.com>
2023-02-16 20:22:31 -0800

    404 page

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import { Button, Container } from "react-bootstrap";
+
+export default function Custom404() {
+    return (
+        <Container className="d-flex flex-column justify-content-center align-items-center" style={{ height: "80vh" }}>
+            <h1 className="primary" style={{ fontSize: "10rem" }}>
+                404
+            </h1>
+            <p>The page you are looking for does not seem to exist.</p>
+            <Button variant="outline-primary" href="/">Home</Button>
+        </Container>
+    );
+}
