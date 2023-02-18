f8d2d999137f060a48b4ede1e39fe3c47ff75bb3
kunpai <kunpai@ucdavis.edu>
2023-02-17 23:24:10 -0800

    Removed Groups and Zipped as filters

--- pages/api/getfilters.js
+++ pages/api/getfilters.js
@@ -4,18 +4,18 @@ export async function getFilters() {
     // get unique categories from resources
     let categories = [...new Set(resources['resources'].map(resource => resource.category))];
     // get unique groups from resources and remove null values
-    let groups = [...new Set(resources['resources'].map(resource => resource.group))].filter(group => group != null);
+    // let groups = [...new Set(resources['resources'].map(resource => resource.group))].filter(group => group != null);
     // get unique architectures from resources
     let architectures = [...new Set(resources['resources'].map(resource => resource.architecture))];
     // get zipped from resources
-    let zippeds = [...new Set(resources['resources'].map(resource => String(resource.is_zipped)))].filter(zipped => zipped != "null");
+    // let zippeds = [...new Set(resources['resources'].map(resource => String(resource.is_zipped)))].filter(zipped => zipped != "null");
     // get gem5_version from resources
     let gem5_versions = [...new Set(resources['resources'].map(resource => resource.gem5_version))].filter(gem5_version => gem5_version != null);
     return {
         category: categories,
-        group: groups,
+       // group: groups,
         architecture: architectures,
-        is_zipped : zippeds,
+        // is_zipped : zippeds,
         gem5_version: gem5_versions
     };
 }
