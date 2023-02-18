1efcc4a7764cefb362a3b4263001327f996b278a
Parth Shah <helloparthshah@gmail.com>
2023-02-17 16:19:45 -0800

    changed config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,8 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  reactStrictMode: true,
   assetPrefix: isProd ? '/gem5-resources-website/' : '',
   images: {
-    loader: 'akamai',
     unoptimized: true,
   },
 }
