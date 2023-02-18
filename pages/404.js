e028d0081f06581df9870806acf3a49296ef45cd
kunpai <kunpai@ucdavis.edu>
2023-02-18 10:11:24 -0800

    Fixed routing bug with Home button in 404 page

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ export default function Custom404() {
                 404
             </h1>
             <p>The page you are looking for does not seem to exist.</p>
-            <Button variant="outline-primary" href="/">Home</Button>
+            <Button variant="outline-primary" href="/gem5-resources-website/">Home</Button>
         </Container>
     );
 }
