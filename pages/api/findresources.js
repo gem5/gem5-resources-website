75c61b553e86cbb3ed8147478d22996da2289791
kunpai <kunpai@ucdavis.edu>
2023-02-20 09:58:09 -0800

    Implemented Damerau-Levenshtein distance algo for Fuzzy Search

--- pages/api/findresources.js
+++ pages/api/findresources.js
@@ -181,3 +181,41 @@ export default async function handler(req, res) {
   let results = await getResourcesMongoDB({ query: query }, {});
   res.status(200).json(results);
 }
+
+
+function damerauLevenshteinDistance(a, b) {
+  if (a.length == 0) return b.length;
+  if (b.length == 0) return a.length;
+  var matrix = [];
+  for (var i = 0; i <= b.length; i++) {
+    matrix[i] = [i];
+  }
+  for (var j = 0; j <= a.length; j++) {
+    matrix[0][j] = j;
+  }
+  for (i = 1; i <= b.length; i++) {
+    for (j = 1; j <= a.length; j++) {
+      if (b.charAt(i - 1) == a.charAt(j - 1)) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] = Math.min(
+          matrix[i - 1][j - 1] + 1, // substitution
+          matrix[i][j - 1] + 1,     // insertion
+          matrix[i - 1][j] + 1      // deletion
+        );
+        if (
+          i > 1 &&
+          j > 1 &&
+          b.charAt(i - 1) == a.charAt(j - 2) &&
+          b.charAt(i - 2) == a.charAt(j - 1)
+        ) {
+          matrix[i][j] = Math.min(
+            matrix[i][j],
+            matrix[i - 2][j - 2] + 1 // transposition
+          );
+        }
+      }
+    }
+  }
+  return matrix[b.length][a.length];
+}
