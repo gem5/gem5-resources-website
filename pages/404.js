1a186be857b88a1aca88365dae9da8ada9452c66
Parth Shah <helloparthshah@gmail.com>
2023-02-20 20:04:48 -0800

    fixed static routing

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,56 @@
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { Button, Container } from "react-bootstrap";
+import { getResource } from "./api/getresource";
+import Resource from "./resources/[id]/[[...page]]";
 
 export default function Custom404() {
+    const router = useRouter()
+    const [error, setError] = useState(false)
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        console.log("404")
+        async function fetchResource() {
+            setLoading(true)
+            // get the url in the form of /resources/[id]/[...page]
+            // print the url and get each part of the url
+            console.log(router.asPath)
+            const url = router.asPath.split("/")
+            if (url.length < 3 && url[1] !== "resources") {
+                setError(true)
+                router.push(`/404`)
+            }
+            const id = url[2]
+            console.log(id)
+            let resource = await getResource(id);
+            console.log(resource)
+            if (resource.error) {
+                setError(true)
+                // change url to 404 without reloading the page
+                router.push(`/404`)
+            }
+            else
+                setError(false)
+            setLoading(false)
+        }
+        if (router.isReady && router.query !== undefined)
+            fetchResource();
+    }, [router.query.id, router.isReady])
+
     return (
-        <Container className="d-flex flex-column justify-content-center align-items-center" style={{ height: "80vh" }}>
-            <h1 className="primary" style={{ fontSize: "10rem" }}>
-                404
-            </h1>
-            <p>The page you are looking for does not seem to exist.</p>
-            <Button variant="outline-primary" href="/gem5-resources-website/">Home</Button>
-        </Container>
+        loading ? <div>Loading...</div> :
+            <>
+                {
+                    error ?
+                        <Container className="d-flex flex-column justify-content-center align-items-center" style={{ height: "80vh" }}>
+                            <h1 className="primary" style={{ fontSize: "10rem" }}>
+                                404
+                            </h1>
+                            <p>The page you are looking for does not seem to exist.</p>
+                            <Button variant="outline-primary" href="/">Home</Button>
+                        </Container>
+                        : <Resource />
+                }
+            </>
     );
 }
--- pages/resources/[id]/[[...page]].js
+++ pages/resources/[id]/[[...page]].js
@@ -2,43 +2,57 @@ import MetaData from '@/components/resource-metadata'
 import Banner from '@/components/resource-banner'
 import Head from 'next/head'
 import { Row, Col, Container } from 'react-bootstrap'
-import { SSRProvider } from "@react-aria/ssr";
 import { getResource } from '../../api/getresource'
 import ResourceTab from '@/components/resource-tab'
-import { fetchResources } from '../../api/resources'
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 function Resource() {
     const [resource, setResource] = useState([])
+    const [loading, setLoading] = useState(true)
     const router = useRouter()
 
     useEffect(() => {
+        console.log("404")
         async function fetchResource() {
-            const id = router.query.id
+            setLoading(true)
+            // get the url in the form of /resources/[id]/[...page]
+            // print the url and get each part of the url
+            console.log(router.asPath)
+            const url = router.asPath.split("/")
+            const id = url[2]
+            console.log(id)
             let resource = await getResource(id);
-            setResource(resource);
+            console.log(resource)
+            if (resource.error) {
+                router.push(`/404`)
+            }
+            else
+                setResource(resource)
+            setLoading(false)
         }
-        fetchResource();
-    }, [router.query.id])
+        if (router.isReady && router.query !== undefined)
+            fetchResource();
+    }, [router.query.id, router.isReady])
 
     return (
-        <SSRProvider>
-            <Head>
-                <title>gem5 resources</title>
-                <meta name="description" content="Find the resource you need" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-            </Head>
-            <Container className='mt-5'>
-                <Row>
-                    <Banner resource={resource ?? {}} />
-                </Row>
-                <Row>
-                    <ResourceTab resource={resource ?? {}} />
-                    <MetaData resource={resource ?? {}} className='ms-5' />
-                </Row>
-            </Container>
-        </SSRProvider >
+        loading ? <div>Loading...</div> :
+            <>
+                <Head>
+                    <title>gem5 resources</title>
+                    <meta name="description" content="Find the resource you need" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                </Head>
+                <Container className='mt-5'>
+                    <Row>
+                        <Banner resource={resource ?? {}} />
+                    </Row>
+                    <Row>
+                        <ResourceTab resource={resource ?? {}} />
+                        <MetaData resource={resource ?? {}} className='ms-5' />
+                    </Row>
+                </Container>
+            </>
     )
 }
 
