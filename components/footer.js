ff46535955bd319571464cf5ec94f6f53f91e752
Parth Shah <helloparthshah@gmail.com>
2023-02-20 22:55:34 -0800

    added footer

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,49 @@
+import Link from "next/link";
+import { Col, Row } from "react-bootstrap";
+
 export default function Footer() {
     return (
         <>
-            <footer>
-                <p></p>
-            </footer>
+            <div className="d-flex flex-row justify-content-center align-items-center bg-light mt-5" style={{ height: "20vh" }}>
+                <Row className="h-100 w-75">
+                    <Col className="text-center primary d-flex flex-column h-100 pt-2 pb-2 gap-1">
+                        <span className="text-muted">gem5 Resources</span>
+                        <Link href="/about">
+                            About
+                        </Link>
+                        <Link href="https://www.gem5.org/publications/">
+                            Publications
+                        </Link>
+                        <Link href="https://www.gem5.org/contributing/">
+                            Contributing
+                        </Link>
+                        <Link href="https://www.gem5.org/governance/">
+                            Governance
+                        </Link>
+                    </Col>
+                    <Col className="text-center primary d-flex flex-column h-100 pt-2 pb-2 gap-1">
+                        <span className="text-muted">Docs</span>
+                        <Link href="https://www.gem5.org/documentation/">
+                            Documentation
+                        </Link>
+                        <Link href="https://gem5.googlesource.com/public/gem5">
+                            Source
+                        </Link>
+                    </Col>
+                    <Col className="text-center primary d-flex flex-column h-100 pt-2 pb-2 gap-1">
+                        <span className="text-muted">Help</span>
+                        <Link href="https://www.gem5.org/search/">
+                            Search
+                        </Link>
+                        <Link href="https://www.gem5.org/mailing_lists/">
+                            Mailing Lists
+                        </Link>
+                        <Link href="https://github.com/Gem5Vision/gem5-resources-website">
+                            Website Source
+                        </Link>
+                    </Col>
+                </Row>
+            </div>
         </>
     )
 }
