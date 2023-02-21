22468e603d4de4af693be408f86aeee70e544397
Parth Shah <helloparthshah@gmail.com>
2023-02-20 18:57:35 -0800

    testing

--- pages/api/getToken.js
+++ pages/api/getToken.js
@@ -1,9 +1,4 @@
-let prevToken = null;
-
 export default async function getToken() {
-    if (prevToken) {
-        return prevToken;
-    }
     let token = await fetch('https://realm.mongodb.com/api/client/v2.0/app/data-ejhjf/auth/providers/api-key/login', {
         method: 'POST',
         headers: {
@@ -16,6 +11,7 @@ export default async function getToken() {
     }).catch(err => console.log(err));
     let tokenJson = await token.json();
     let accessToken = tokenJson['access_token'];
-    prevToken = accessToken;
+    if (accessToken)
+        console.log("Successfully got access token");
     return accessToken;
 }
