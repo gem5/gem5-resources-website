2ec0119b1e6621f27c4528654b7a3d5a56f555fa
Harshil2107 <harshilp2107@gmail.com>
2023-02-25 16:43:50 -0800

    added suppourt ofr sorting by versions in mongoDB

--- pages/api/findresources.js
+++ pages/api/findresources.js
@@ -9,7 +9,7 @@ import { version } from "nprogress";
  * @param {json} queryObject The query object.
  * @param {json} filters The filters object.
  * @returns {JSX.Element} The JSX element to be rendered.
-*/
+ */
 async function getResourcesMongoDB(queryObject, filters) {
   // pass queryObject by value
   queryObject = { ...queryObject };
@@ -32,112 +32,128 @@ async function getResourcesMongoDB(queryObject, filters) {
     console.log(queryObject.sort);
     switch (queryObject.sort) {
       case "date":
-        return { "date": -1 };
+        return { date: -1 };
       case "name":
-        return { "id": 1 };
+        return { id: 1 };
       case "version":
-        return { "gem5_version": -1 };
+        return { "ver.k": -1 };
       case "id_asc":
-        return { "id": 1 };
+        return { id: 1 };
       case "id_desc":
-        return { "id": -1 };
+        return { id: -1 };
       case "default":
         return {
-          "_id": -1
+          _id: -1,
         };
       default:
         return {
-          "score": { "$meta": "textScore" }
+          score: { $meta: "textScore" },
         };
     }
   }
   const access_token = await getToken();
-  if (queryObject.query.trim() === '') {
+  if (queryObject.query.trim() === "") {
     if (queryObject.sort === "relevance") {
       queryObject.sort = "default";
     }
-    const res1 = await fetch('https://us-west-2.aws.data.mongodb-api.com/app/data-ejhjf/endpoint/data/v1/action/aggregate', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        // 'api-key': 'pKkhRJGJaQ3NdJyDt69u4GPGQTDUIhHlx4a3lrKUNx2hxuc8uba8NrP3IVRvlzlo',
-        'Access-Control-Request-Headers': '*',
-        // 'origin': 'https://gem5vision.github.io',
-        "Authorization": "Bearer " + access_token,
-      },
-      // also apply filters on
-      body: JSON.stringify({
-        "dataSource": "gem5-vision",
-        "database": "gem5-vision",
-        "collection": "resources",
-        "pipeline": [
-          {
-            "$match": {
-              "$and": [
-                { "category": { "$in": queryObject.category || [] } },
-                { "architecture": { "$in": queryObject.architecture || [] } },
-                // check if any keys of versions is in queryObject.versions
-              ]
-            },
-          },
-          {
-            "$sort": getSort()
-          }
-        ]
-      })
-    }
-    ).catch(err => console.log(err));
-    const resources = await res1.json();
-    return resources['documents'];
-  } else {
-    const res = await fetch('https://us-west-2.aws.data.mongodb-api.com/app/data-ejhjf/endpoint/data/v1/action/aggregate', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        // 'api-key': 'pKkhRJGJaQ3NdJyDt69u4GPGQTDUIhHlx4a3lrKUNx2hxuc8uba8NrP3IVRvlzlo',
-        'Access-Control-Request-Headers': '*',
-        // 'origin': 'https://gem5vision.github.io',
-        "Authorization": "Bearer " + access_token,
-      },
-      // also apply filters on
-      body: JSON.stringify({
-        "dataSource": "gem5-vision",
-        "database": "gem5-vision",
-        "collection": "resources",
-        "pipeline": [
-          {
-            "$search": {
-              "text": {
-                "query": queryObject.query,
-                "path": ["id", "description", "resources"],
-                "fuzzy": {
-                  "maxEdits": 2,
-                  "maxExpansions": 100
-                }
+    const res1 = await fetch(
+      "https://us-west-2.aws.data.mongodb-api.com/app/data-ejhjf/endpoint/data/v1/action/aggregate",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // 'api-key': 'pKkhRJGJaQ3NdJyDt69u4GPGQTDUIhHlx4a3lrKUNx2hxuc8uba8NrP3IVRvlzlo',
+          "Access-Control-Request-Headers": "*",
+          // 'origin': 'https://gem5vision.github.io',
+          Authorization: "Bearer " + access_token,
+        },
+        // also apply filters on
+        body: JSON.stringify({
+          dataSource: "gem5-vision",
+          database: "gem5-vision",
+          collection: "resources",
+          pipeline: [
+            {
+              $match: {
+                $and: [
+                  { category: { $in: queryObject.category || [] } },
+                  { architecture: { $in: queryObject.architecture || [] } },
+                  // check if any keys of versions is in queryObject.versions
+                ],
               },
-            }
-          },
-
-          {
-            "$sort": getSort()
-          },
-          {
-            "$match": {
-              "$and": [
-                { "category": { "$in": queryObject.category || [] } },
-                { "architecture": { "$in": queryObject.architecture || [] } },
-              ]
             },
-          },
-
-        ],
-      })
-    }).catch(err => console.log(err));
+            {
+              $sort: getSort(),
+            },
+          ],
+        }),
+      }
+    ).catch((err) => console.log(err));
+    const resources = await res1.json();
+    return resources["documents"];
+  } else {
+    const res = await fetch(
+      "https://us-west-2.aws.data.mongodb-api.com/app/data-ejhjf/endpoint/data/v1/action/aggregate",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // 'api-key': 'pKkhRJGJaQ3NdJyDt69u4GPGQTDUIhHlx4a3lrKUNx2hxuc8uba8NrP3IVRvlzlo',
+          "Access-Control-Request-Headers": "*",
+          // 'origin': 'https://gem5vision.github.io',
+          Authorization: "Bearer " + access_token,
+        },
+        // also apply filters on
+        body: JSON.stringify({
+          dataSource: "gem5-vision",
+          database: "gem5-vision",
+          collection: "resources",
+          pipeline: [
+            {
+              $search: {
+                text: {
+                  query: queryObject.query,
+                  path: ["id", "description", "resources"],
+                  fuzzy: {
+                    maxEdits: 2,
+                    maxExpansions: 100,
+                  },
+                },
+              },
+            },
+            {
+              $addFields: {
+                ver: {
+                  $objectToArray: "$versions",
+                },
+              },
+            },
+            {
+              $unwind: "$ver",
+            },
+            {
+              $sort: getSort(),
+            },
+            {
+              $unset: "ver",
+            },
+            {
+              $match: {
+                $and: [
+                  { category: { $in: queryObject.category || [] } },
+                  { architecture: { $in: queryObject.architecture || [] } },
+                ],
+              },
+            },
+          ],
+        }),
+      }
+    ).catch((err) => console.log(err));
     const resources = await res.json();
     console.log(resources);
     for (let filter in queryObject) {
-      if (filter === 'versions') {
-        resources['documents'] = resources['documents'].filter(resource => {
+      if (filter === "versions") {
+        resources["documents"] = resources["documents"].filter((resource) => {
           for (let version in queryObject[filter]) {
             if (resource.versions[queryObject[filter][version]]) {
               return true;
@@ -147,7 +163,7 @@ async function getResourcesMongoDB(queryObject, filters) {
         });
       }
     }
-    return resources['documents']
+    return resources["documents"];
   }
 }
 
@@ -157,28 +173,38 @@ async function getResourcesMongoDB(queryObject, filters) {
  * @description Fetches the resources based on the query object from the JSON file.
  * @param {json} queryObject The query object.
  * @returns {JSX.Element} The JSX element to be rendered.
-*/
+ */
 async function getResourcesJSON(queryObject) {
   const resources = await fetchResources();
   const query = queryObject.query.trim();
   const keywords = query.split(" ");
-  let results = resources.filter(resource => {
-    let idMatches = keywords.filter(keyword => resource.id.toLowerCase().includes(keyword.toLowerCase())).length;
-    let descMatches = keywords.filter(keyword => resource.description.toLowerCase().includes(keyword.toLowerCase())).length;
+  let results = resources.filter((resource) => {
+    let idMatches = keywords.filter((keyword) =>
+      resource.id.toLowerCase().includes(keyword.toLowerCase())
+    ).length;
+    let descMatches = keywords.filter((keyword) =>
+      resource.description.toLowerCase().includes(keyword.toLowerCase())
+    ).length;
     let resMatches = 0;
-    if (resource.resources) { // only search if resource.resources exists
+    if (resource.resources) {
+      // only search if resource.resources exists
       const resourceJSON = JSON.stringify(resource.resources).toLowerCase();
-      resMatches = keywords.filter(keyword => resourceJSON.includes(keyword.toLowerCase())).length;
+      resMatches = keywords.filter((keyword) =>
+        resourceJSON.includes(keyword.toLowerCase())
+      ).length;
     }
     let totalMatches = idMatches + descMatches + resMatches;
     if (totalMatches === 0) {
-      let idDistances = keywords.map(keyword => {
+      let idDistances = keywords.map((keyword) => {
         const keywordLower = keyword.toLowerCase();
-        return Math.min(...resource.id.toLowerCase()
-          .split("-")
-          .map(idPart => damerauLevenshteinDistance(keywordLower, idPart)));
+        return Math.min(
+          ...resource.id
+            .toLowerCase()
+            .split("-")
+            .map((idPart) => damerauLevenshteinDistance(keywordLower, idPart))
+        );
       });
-      idMatches = idDistances.filter(d => d < 3).length;
+      idMatches = idDistances.filter((d) => d < 3).length;
 
       // let descDistances = keywords.map(keyword => {
       //   const keywordLower = keyword.toLowerCase();
@@ -187,15 +213,18 @@ async function getResourcesJSON(queryObject) {
       //     .map(descPart => damerauLevenshteinDistance(keywordLower, descPart)));
       // });
       // descMatches = descDistances.filter(d => d < 3).length;
-      if (resource.resources) { // only search if resource.resources exists
+      if (resource.resources) {
+        // only search if resource.resources exists
         const resourceJSON = JSON.stringify(resource.resources).toLowerCase();
-        resMatches = keywords.filter(keyword => resourceJSON.includes(keyword.toLowerCase())).length;
+        resMatches = keywords.filter((keyword) =>
+          resourceJSON.includes(keyword.toLowerCase())
+        ).length;
       }
       totalMatches = idMatches + descMatches + resMatches;
     }
-    resource['totalMatches'] = totalMatches;
+    resource["totalMatches"] = totalMatches;
     return totalMatches > 0;
-  })
+  });
   console.log(queryObject);
   if (queryObject.sort) {
     switch (queryObject.sort) {
@@ -210,8 +239,14 @@ async function getResourcesJSON(queryObject) {
         break;
       case "version":
         results = results.sort((a, b) => {
-          const aVersion = Object.keys(a.versions).length > 1 ? Object.keys(a.versions)[1] : Object.keys(a.versions)[0];
-          const bVersion = Object.keys(b.versions).length > 1 ? Object.keys(b.versions)[1] : Object.keys(b.versions)[0];
+          const aVersion =
+            Object.keys(a.versions).length > 1
+              ? Object.keys(a.versions)[1]
+              : Object.keys(a.versions)[0];
+          const bVersion =
+            Object.keys(b.versions).length > 1
+              ? Object.keys(b.versions)[1]
+              : Object.keys(b.versions)[0];
           return bVersion.localeCompare(aVersion);
         });
         console.log(results);
@@ -224,8 +259,8 @@ async function getResourcesJSON(queryObject) {
   }
 
   for (let filter in queryObject) {
-    if (filter === 'versions') {
-      results = results.filter(resource => {
+    if (filter === "versions") {
+      results = results.filter((resource) => {
         for (let version in queryObject[filter]) {
           if (resource.versions[queryObject[filter][version]]) {
             return true;
@@ -233,9 +268,10 @@ async function getResourcesJSON(queryObject) {
         }
         return false;
       });
-    }
-    else if (filter !== "query" && filter !== "sort") {
-      results = results.filter(resource => queryObject[filter].includes(String(resource[filter])));
+    } else if (filter !== "query" && filter !== "sort") {
+      results = results.filter((resource) =>
+        queryObject[filter].includes(String(resource[filter]))
+      );
     }
   }
   return results;
@@ -248,20 +284,28 @@ async function getResourcesJSON(queryObject) {
  * @param {json} queryObject The query object.
  * @param {json} filters The filters to be applied.
  * @returns {json} The resources in JSON format.
-*/
-export async function getResources(queryObject, filters, currentPage, pageSize) {
+ */
+export async function getResources(
+  queryObject,
+  filters,
+  currentPage,
+  pageSize
+) {
   let resources;
   // if (process.env.IS_MONGODB_ENABLED === "true") {
   resources = await getResourcesMongoDB(queryObject, filters);
   // } else {
   // resources = await getResourcesJSON(queryObject);
   let total = resources.length;
-  resources = resources.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+  resources = resources.slice(
+    (currentPage - 1) * pageSize,
+    currentPage * pageSize
+  );
   // }
   return {
     resources: resources,
-    total: total
-  }
+    total: total,
+  };
 }
 
 export default async function handler(req, res) {
@@ -276,7 +320,7 @@ export default async function handler(req, res) {
  * @helper
  * @description Calculates the Damerau-Levenshtein distance between two strings. Used for fuzzy search.
  * @returns {number} The Damerau-Levenshtein distance between the two strings.
-*/
+ */
 function damerauLevenshteinDistance(a, b) {
   if (a.length == 0) return b.length;
   if (b.length == 0) return a.length;
@@ -294,8 +338,8 @@ function damerauLevenshteinDistance(a, b) {
       } else {
         matrix[i][j] = Math.min(
           matrix[i - 1][j - 1] + 1, // substitution
-          matrix[i][j - 1] + 1,     // insertion
-          matrix[i - 1][j] + 1      // deletion
+          matrix[i][j - 1] + 1, // insertion
+          matrix[i - 1][j] + 1 // deletion
         );
         if (
           i > 1 &&
