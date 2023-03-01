5b2d03ec84bbb8490c60c6b1c0f4358a73794980
Parth Shah <helloparthshah@gmail.com>
2023-02-28 17:44:12 -0800

    changes to the ui

--- components/searchresult.js
+++ components/searchresult.js
@@ -6,6 +6,7 @@ import arm from "/public/arm.png";
 import sparc from "/public/sparc.png";
 import mips from "/public/mips.png";
 import power from "/public/power.png";
+import { Badge, Col, Row } from "react-bootstrap";
 
 /**
  * @component
@@ -38,12 +39,46 @@ export default function SearchResult({ resource }) {
         <div className="search-result">
             <Link href={'/resources/' + resource.id} style={{ textDecoration: 'none', color: 'inherit' }}>
                 <div className="search-result__title">
-                    <h4>{resource.id}</h4>
+                    <div className="d-flex flex-row justify-content-between align-items-start">
+                        <h4>{resource.id}</h4>
+                        <Row>
+                            <Col className="border-end">
+                                <div className="d-flex flex-column justify-content-center align-items-center">
+                                    <h5 className="primary">
+                                        {Math.floor(Math.random() * 100)}
+                                    </h5>
+                                    <p className="text-muted" style={{ fontSize: '0.8rem' }}>
+                                        LIKES
+                                    </p>
+                                </div>
+                            </Col>
+                            <Col className="border-end">
+                                <div className="d-flex flex-column justify-content-center align-items-center">
+                                    <h5 className="primary">
+                                        {Math.floor(Math.random() * 10000)}
+                                    </h5>
+                                    <p className="text-muted" style={{ fontSize: '0.8rem' }}>
+                                        VIEWS
+                                    </p>
+                                </div>
+                            </Col>
+                            <Col>
+                                <div className="d-flex flex-column justify-content-center align-items-center">
+                                    <h5 className="primary">
+                                        {Math.floor(Math.random() * 100)}
+                                    </h5>
+                                    <p className="text-muted" style={{ fontSize: '0.8rem' }}>
+                                        DOWNLOADS
+                                    </p>
+                                </div>
+                            </Col>
+                        </Row>
+                    </div>
                 </div>
                 <div className="search-result__description">
                     <p>{resource.description}</p>
                 </div>
-                <div className="d-flex align-items-center gap-2">
+                <div className='d-flex gap-3'>
                     <div className="d-flex gap-1 align-items-center">
                         <Image
                             src={getIcon(resource.architecture)}
@@ -55,12 +90,36 @@ export default function SearchResult({ resource }) {
                             {resource.architecture ?? "Unknown"}
                         </p>
                     </div>
-                    <p className="text-capitalize fw-light">
-                        {resource.category}
-                    </p>
-                    <p className="fw-light">
-                        {resource.license ?? "Unknown"}
-                    </p>
+                    <div className='d-flex flex-row gap-1'>
+                        <p className="text-capitalize font-weight-light ">
+                            {resource.category}
+                        </p>
+                    </div>
+                    <div className='d-flex flex-row gap-1'>
+                        <h6 style={{ lineHeight: 'inherit', margin: '0' }}>
+                            v
+                        </h6>
+                        <Link className='text-decoration-none text-black' href={'/resources?q=versions:' + (resource.versions.length > 1 ? resource.versions[1].version : resource.versions[0].version)}>
+                            {
+                                resource.versions.length > 1 ? resource.versions[1].version : resource.versions[0].version
+                            }
+                        </Link>
+                    </div>
+                    <div className='d-flex flex-row gap-1'>
+                        {
+                            resource.tags ? resource.tags.map((tag, index) => {
+                                return (
+                                    <Link key={tag}
+                                        href={'/resources?q=tag:' + tag}
+                                    >
+                                        <Badge pill bg='primary' >
+                                            {tag}
+                                        </Badge>
+                                    </Link>
+                                )
+                            }) : ""
+                        }
+                    </div>
                 </div>
             </Link>
         </div>
