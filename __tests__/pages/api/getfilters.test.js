53c88241dfc57cc8f5fbc4254f576cf5bafbfc44
kunpai <kunpai@ucdavis.edu>
2023-04-23 10:48:33 -0700

    getFilters test works

--- __tests__/pages/api/getfilters.test.js
+++ __tests__/pages/api/getfilters.test.js
@@ -1,18 +1,57 @@
 import { getFilters } from "@/pages/api/getfilters";
+import resources from "./resources.json"
+import { get } from "fetch-mock";
 
 const originalEnv = process.env;
 
-global.fetch = jest.fn(() =>
-    Promise.resolve({
-        json: () => Promise.resolve([]),
+global.fetch = jest.fn((url) => {
+    console.log(url);
+    if (url.includes("data.mongodb-api")) {
+        return Promise.resolve({
+            json: () => Promise.resolve({
+                'documents': [
+                    {
+                        "category": ["simpoint", "file"],
+                        "id": "batman",
+                        "architecture": ["X86", "ARM", null],
+                        "tags": [],
+                        "resource_version": "1.0.0",
+                        "gem5_versions": ["22.0"],
+                        "database": "db1",
+                    },
+                ]
+            }),
+        })
+    }
+
+    if (url.includes("resources.json")) {
+        return Promise.resolve({
+            json: () => Promise.resolve(resources),
+        })
+    }
+
+    if (url.includes("realm.mongodb.com")) {
+        return Promise.resolve({
+            json: () => Promise.resolve({
+                "access_token": ""
+            }),
+        })
+    }
+
+    return Promise.resolve({
+        json: () => Promise.resolve({
+            "error": "Resource not found"
+        }),
     })
-);
+});
+
 
 describe('getFilters', () => {
     beforeEach(() => {
         jest.resetModules();
         process.env = {
             ...originalEnv,
+            BASE_PATH: '',
             PRIVATE_RESOURCES: {
                 "db1": {
                     url: "resources.json",
@@ -26,51 +65,37 @@ describe('getFilters', () => {
         process.env = originalEnv;
     });
 
-    test('returns empty object if no private resources are defined', async () => {
-        // Arrange
-        process.env.PRIVATE_RESOURCES = {};
+    test("json filters", async () => {
+        let result = await getFilters();
+        expect(result).toEqual({
+            "architecture": ["X86"],
+            "category": ["binary", "file", "simpoint"],
+            "database": ["db1"],
+            "gem5_versions": ["23.1", "23.0"]
+        });
+    });
 
-        // Act
-        const filters = await getFilters();
-
-        // Assert
-        expect(filters).toEqual({
-            category: [],
-            architecture: [],
-            gem5_versions: [],
+    test("mongodb filters", async () => {
+        process.env = {
+            ...originalEnv,
+            PRIVATE_RESOURCES: {
+                "db1": {
+                    dataSource: "gem5-vision",
+                    database: "gem5-vision",
+                    collection: "versions_test",
+                    url: "https://data.mongodb-api.com/app/data-ejhjf/endpoint/data/v1",
+                    name: "data-ejhjf",
+                    apiKey: "pKkhRJGJaQ3NdJyDt69u4GPGQTDUIhHlx4a3lrKUNx2hxuc8uba8NrP3IVRvlzlo",
+                    isMongo: true,
+                },
+            }
+        };
+        let result = await getFilters();
+        expect(result).toEqual({
+            architecture: ["ARM","X86"],
+            category: ["file", "simpoint"],
+            gem5_versions: ["22.0"],
+            database: ["db1"],
         });
     });
 });
-
-
-
-//   it('returns the correct filters', async () => {
-//     // Mock the getFiltersMongoDB and getFiltersJSON functions
-//     const getFiltersMongoDB = jest.fn().mockResolvedValue({
-//       category: ['cat1', 'cat2'],
-//       architecture: ['arch1', 'arch2'],
-//       gem5_versions: ['v1', 'v2'],
-//     });
-//     const getFiltersJSON = jest.fn().mockResolvedValue({
-//       category: ['cat2', 'cat3'],
-//       architecture: ['arch2', 'arch3'],
-//       gem5_versions: ['v2', 'v3'],
-//     });
-
-//     // Call the getFilters function and check the result
-//     const result = await getFilters();
-
-//     expect(result).toEqual({
-//       category: ['cat1', 'cat2', 'cat3'],
-//       architecture: ['arch1', 'arch2', 'arch3'],
-//       gem5_versions: ['v3', 'v2', 'v1'],
-//       database: ['resource1', 'resource2'],
-//     });
-//     expect(getFiltersMongoDB).toHaveBeenCalledTimes(1);
-//     expect(getFiltersMongoDB).toHaveBeenCalledWith('resource1');
-//     expect(getFiltersJSON).toHaveBeenCalledTimes(1);
-//     expect(getFiltersJSON).toHaveBeenCalledWith('resource2');
-//   });
-
-
-// });
