e0b34278aa0015e3aefc29e5998b5f8f24898e79
Parth Shah <helloparthshah@gmail.com>
2023-05-03 21:19:25 -0700

    Updated to use session storage for user token
    Co-authored-by: kunpai <kunpai@ucdavis.edu>

--- pages/api/mongodb/getToken.js
+++ pages/api/mongodb/getToken.js
@@ -29,5 +29,18 @@ export default async function getToken(key = null) {
         key = Object.keys(process.env.PRIVATE_RESOURCES)[0];
     }
     let privateENV = process.env.PRIVATE_RESOURCES[key];
-    return await getAccessToken(`https://realm.mongodb.com/api/client/v2.0/app/${privateENV.name}/auth/providers/api-key/login`, privateENV.apiKey);
+    let token = sessionStorage.getItem('token');
+    if (token) {
+        token = JSON.parse(token);
+    } else {
+        token = {};
+    }
+    if (token && token[key]) {
+        return token[key];
+    }
+    else {
+        token[key] = await getAccessToken(`https://realm.mongodb.com/api/client/v2.0/app/${privateENV.name}/auth/providers/api-key/login`, privateENV.apiKey);
+        sessionStorage.setItem('token', JSON.stringify(token));
+        return token[key];
+    }
 }
