44219dcd297f31fea387613a394ea77432a3d46c
Parth Shah <helloparthshah@gmail.com>
2023-05-30 15:06:45 -0700

    Hotfix for static url

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
 // This file is used to configure Next.js
 const config = require('./gem5.config.json');
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-let assetPrefix = undefined
-let basePath = ''
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  basePath = `/${repo}`
-}
-let isProd = process.env.NODE_ENV === 'production'
-
-if (!isProd) {
-  assetPrefix = undefined;
-  basePath = '';
-}
+assetPrefix = undefined;
+basePath = '';
 
 module.exports = {
   assetPrefix: assetPrefix,
